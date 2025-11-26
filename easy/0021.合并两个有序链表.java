4a4c62da3583beb0170855a96cb04374959ecc4b
siemonsele400-hue <siemonsele400@gmail.com>
2025-11-26 16:39:08 +0800

    Implement mergeTwoLists function in 合并两个有序链表
    Updated the file to implement the mergeTwoLists function for merging two sorted linked lists.

--- easy/0021.合并两个有序链表.java
+++ easy/0021.合并两个有序链表.java
@@ -1,22 +1,31 @@
 /**
- * 郎新博 | 2025.11.26 Day1 第2题
- * 20. 有效的括号
- * https://leetcode.cn/problems/valid-parentheses/
- * 栈的经典应用 一次AC
+ * 郎新博 | 2025.11.26 Day1 第3题
+ * 21. 合并两个有序链表
+ * https://leetcode.cn/problems/merge-two-sorted-lists/
+ * 虚拟头结点 + 双指针
  */
-import java.util.Stack;
-
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(')      stack.push(')');
-            else if (c == '[') stack.push(']');
-            else if (c == '{') stack.push('}');
-            else if (stack.isEmpty() || stack.pop() != c) {
-                return false;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = L1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
             }
+            cur = cur.next;
         }
-        return stack.isEmpty();
+        cur.next = l1 != null ? l1 : l2;
+        return dummy.next;
     }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
